9b942e280afac037633b827e6956ae052b291727
Michael Lohier <lohiermichael@gmail.com>
2020-08-08 11:36:40 +0800

    Remove tutorials

--- save_data.py
+++ save_data.py
@@ -12,24 +12,11 @@ if __name__ == '__main__':
         channel = Channel(id=channel_id)
         # Make a file with all playlists in the channel
         playlists = channel.playlists
-        playlists.to_csv(f'./channel_{channel_id}/playlists.csv')
+        Path(
+            f'./channel_{channel_id}/playlists').mkdir(parents=True, exist_ok=True)
+        playlists.to_csv(f'./channel_{channel_id}/playlists/playlists.csv')
         for _, playlist in playlists.iterrows():
             playlist = Playlist(id=playlist.id, title=playlist.title)
             playlist_items = playlist.items
             playlist_items.to_csv(
                 f'./channel_{channel_id}/{playlist.title}.csv')
-
-
-# # Make
-# playlists.to_csv('playlists.csv')
-
-# playlist_test_id = playlists[playlists['title']
-#                              == 'Jazz music']['id'].item()
-# # print(playlist_test_id)
-# playlist_test = Playlist(id=playlist_test_id)
-# items_test = playlist_test.items
-# print(len(items_test))
-# items_test.to_csv('items.csv')
-
-# with open('res.json', 'w') as f:
-#     json.dump(response, f)
