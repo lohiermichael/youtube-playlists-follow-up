b7b4560c6ef3d88515181db832dea42ad7d7d225
Michael Lohier <lohiermichael@gmail.com>
2020-08-18 14:52:19 +0800

    Handle old and new elements

--- compare_versions.py
+++ compare_versions.py
@@ -10,32 +10,32 @@ import pandas as pd
 from save_data import save_data
 
 
-def make_summary(old_L: list, recent_L: list, print_of: bool = True) -> Tuple[list]:
-    common_elements = [element for element in old_L if element in recent_L]
+def make_summary(old_L: list, new_L: list, print_of: bool = True) -> Tuple[list]:
+    common_elements = [element for element in old_L if element in new_L]
     elements_old_only = [
-        element for element in old_L if element not in recent_L]
-    elements_recent_only = [
-        element for element in recent_L if element not in old_L]
+        element for element in old_L if element not in new_L]
+    elements_new_only = [
+        element for element in new_L if element not in old_L]
 
     # Summary
     if print_of:
         print(f'In common: {len(common_elements)}')
         print(common_elements)
         print('\n')
-        print(f'In old and not in recent: {len(elements_old_only)}')
+        print(f'In old and not in new: {len(elements_old_only)}')
         print(elements_old_only)
         print('\n')
-        print(f'In recent and not in old: {len(elements_recent_only)}')
-        print(elements_recent_only)
+        print(f'In new and not in old: {len(elements_new_only)}')
+        print(elements_new_only)
         print('\n')
 
-    return common_elements, elements_old_only, elements_recent_only
+    return common_elements, elements_old_only, elements_new_only
 
 
 class CompareVersions:
-    def __init__(self, old_version, recent_version):
+    def __init__(self, old_version, new_version):
         self.old_version = old_version
-        self.recent_version = recent_version
+        self.new_version = new_version
         self.summary = {}
         self._run_comparisions()
 
@@ -47,7 +47,7 @@ class CompareVersions:
         self.compare_playlists()
         self._save_comparison_results()
         print('Compare playlist items')
-        self.compare_playlist_items()
+        self.compare_common_playlist_items()
         self._save_comparison_results()
 
     def _save_comparison_results(self):
@@ -55,19 +55,73 @@ class CompareVersions:
             json.dump(self.summary, f)
 
     def compare_channels(self):
-        old_channels = os.listdir(self.old_version)
-        recent_channels = os.listdir(self.recent_version)
+        self.old_channels = os.listdir(self.old_version)
+        self.new_channels = os.listdir(self.new_version)
+
         self.summary['common_channels'], self.summary['old_channels'], self.summary['new_channels'] = make_summary(
-            old_L=old_channels, recent_L=recent_channels, print_of=False)
+            old_L=self.old_channels, new_L=self.new_channels, print_of=False)
+
+        # Store elements old channels
+        self.store_all_elements_channels(type_channels='old_channels')
+
+        # Store new channels
+        self.store_all_elements_channels(type_channels='new_channels')
+
+    def store_all_elements_channels(self, type_channels: str):
+
+        if type_channels == 'old_channels':
+            list_channels = self.old_channels
+            version = self.old_version
+        elif type_channels == 'new_channels':
+            list_channels = self.new_channels
+            version = self.new_version
+        else:
+            raise AttributeError(
+                f"{type_channels} does not exist, it must be  'old_channels' or 'new_channels'")
+
+        # Store the old channel names
+        list_channels = self.summary[type_channels]
+        self.summary[type_channels] = {}
+
+        # Store the playlists
+        for channel in list_channels:
+            list_playlists = os.listdir(f'{self.old_version}/{channel}')
+            self.summary[type_channels][channel] = {}
+            # Store the items
+            for playlist in list_playlists:
+                df_playlist = pd.read_csv(
+                    f'./{version}/{channel}/{playlist}.csv')
+                self.summary[type_channels][channel][playlist] = list(
+                    df_playlist.title)
 
     def compare_playlists(self):
-        # Store elements new channels
-        # self.store_elements_channels(self, type_channel='old_channels') TODO
 
         # Playlists in common
         self.compare_common_playlists()
 
-        # Store old channels
+        # Store elements old playlists
+        self.store_all_elements_playlists(type_playlists='old_playlists')
+
+        # Store elements old playlists
+        self.store_all_elements_playlists(type_playlists='new_playlists')
+
+    def store_all_elements_playlists(self, type_playlists: str):
+        if type_playlists == 'old_playlists':
+            version = self.old_version
+        elif type_playlists == 'new_playlists':
+            version = self.new_version
+        else:
+            raise AttributeError(
+                f"{type_playlists} does not exist, it must be  'old_playlists' or 'new_playlists'")
+
+        for common_channel in self.summary['common_channels']:
+            list_playlists = self.summary['common_channels'][common_channel][type_playlists]
+            self.summary['common_channels'][common_channel][type_playlists] = {}
+            for playlist in list_playlists:
+                df_playlist = pd.read_csv(
+                    f'./{version}/{common_channel}/{playlist}.csv')
+                self.summary['common_channels'][common_channel][type_playlists][playlist] = list(
+                    df_playlist.title)
 
     def compare_common_playlists(self):
 
@@ -77,17 +131,17 @@ class CompareVersions:
         for channel in list_common_channels:
             self.summary['common_channels'][channel] = {}
             old_playlists = os.listdir(f'{self.old_version}/{channel}')
-            recent_playlists = os.listdir(f'{self.recent_version}/{channel}')
+            new_playlists = os.listdir(f'{self.new_version}/{channel}')
             common_playlists, old_playlists, new_playlists = make_summary(
-                old_L=old_playlists, recent_L=recent_playlists, print_of=False)
+                old_L=old_playlists, new_L=new_playlists, print_of=False)
 
             # Remove .csv
             common_playlists = list(map(lambda element: element.replace(
                 '.csv', ''), common_playlists))
             old_playlists = list(map(lambda element: element.replace(
                 '.csv', ''), old_playlists))
-            recent_playlists = list(map(lambda element: element.replace(
-                '.csv', ''), recent_playlists))
+            new_playlists = list(map(lambda element: element.replace(
+                '.csv', ''), new_playlists))
 
             # Remove playlist
             common_playlists.remove('playlists')
@@ -95,14 +149,6 @@ class CompareVersions:
             self.summary['common_channels'][channel]['common_playlists'] = common_playlists
             self.summary['common_channels'][channel]['old_playlists'] = old_playlists
             self.summary['common_channels'][channel]['new_playlists'] = new_playlists
-
-    def compare_playlist_items(self):
-        # Store old playlists
-
-        # Playlists in common
-        self.compare_common_playlist_items()
-
-        # Store new playlists
 
     def compare_common_playlist_items(self):
         for channel in self.summary['common_channels']:
@@ -113,10 +159,10 @@ class CompareVersions:
                 }
                 df_old_playlist = pd.read_csv(
                     f'./{self.old_version}/{channel}/{playlist}.csv')
-                df_recent_playlist = pd.read_csv(
-                    f'./{self.recent_version}/{channel}/{playlist}.csv')
+                df_new_playlist = pd.read_csv(
+                    f'./{self.new_version}/{channel}/{playlist}.csv')
                 common_items, old_items, new_items = make_summary(
-                    old_L=list(df_old_playlist.title), recent_L=list(df_recent_playlist.title), print_of=False)
+                    old_L=list(df_old_playlist.title), new_L=list(df_new_playlist.title), print_of=False)
                 self.summary['common_channels'][channel]['common_playlists'][playlist]['common_items'] = common_items
                 self.summary['common_channels'][channel]['common_playlists'][playlist]['old_items'] = old_items
                 self.summary['common_channels'][channel]['common_playlists'][playlist]['new_items'] = new_items
@@ -131,7 +177,7 @@ if __name__ == "__main__":
         now = datetime.now()
 
     with open('versions.txt', 'r') as f:
-        print('Download the most recent version of the data')
+        print('Download the most new version of the data')
         old_time = f.read().split('\n')[0]
     save_data(time=now, first_time=False)
 
@@ -139,7 +185,7 @@ if __name__ == "__main__":
     try:
         print('compare the two versions')
         summary_comparison = CompareVersions(
-            old_version=f'{old_time}', recent_version=f'{now}').summary
+            old_version=f'{old_time}', new_version=f'{now}').summary
 
         # Analyze summary comparison
 
