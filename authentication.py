49a14dbd07ef6d00ad442ea7e99877328aeb277a
Michael Lohier <lohiermichael@gmail.com>
2020-08-27 11:47:12 +0800

    Change the Authentication object

--- authentication.py
+++ authentication.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+from shutil import copyfile
 from pathlib import Path
 
 
@@ -12,39 +13,42 @@ from config import FOLDER_CHANNELS
 
 class Authentication:
     def __init__(self,
-                 new_client_secrets: bool = True,
-                 channel_id: str = None,
-                 client_secrets_file: str = '{FOLDER_CHANNELS}/new_channel/client_secret.json'):
-        """Will save in the files the auth credentials if not already done.
-        The important object of Authentication is youtube, which will allow you to request the Youtube data  API.
+                 first_time: bool = None,
+                 new_client_secrets: bool = None,
+                 channel_id_secrets: str = None,
+                 channel_id: str: None):
 
-        Args:
-            first_time (bool): If set to True, it will request an access to your channel. Defaults to True.
-            channel_id (str, optional): If first_time is False, it will get the authentication from the saved files under channel Id. Defaults to None if first_time is True.
-        """
+        # Conditions on the inputs: there are three cases
+    self.case_first_time_reuse_secrets = first_time and not new_client_secrets and channel_id_secrets
+    self.case_first_time_new_secrets = first_time and new_client_secrets
+    self.case_already_stored = not first_time
 
-        self.channel_id = channel_id
+    assert self.case_first_time_reuse_secrets or self.case_first_time_new_secrets or self.case_already_stored, "You are not initializing the inputs correctly"
 
-        if self.channel_id:
-            self.client_secrets_file = f'{FOLDER_CHANNELS}/{channel_id}/client_secrets.json'
+    self.scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+    self.api_service_name = 'youtube'
+    self.api_version = 'v3'
 
-        # The file of the credentials from which the channel has been created if already exists
-        # It is temporarily saved under new channel, as we don't know the channel id yes
-        self.client_secrets_file = f'{FOLDER_CHANNELS}/new_channel/client_secrets.json'
+    self.first_time = first_time
+    self.new_client_secrets = new_client_secrets
+    self.channel_id_secrets = channel_id_secrets
+    self.channel_id = channel_id
+    self.client_secrets_file = self._define_client_secrets_file()
 
-        self.scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
-        self.api_service_name = 'youtube'
-        self.api_version = 'v3'
+    if self.first_time:
+        self._get_authenticated_first_time()
+        self._move_client_secrets_file()
+    else:
+        self._get_authenticated_from_file()
 
-        if new_client_secrets:
-            self._get_authenticated_first_time()
-            # Move the client_secrets_file under its channel id folder
-            new_destination = f'{FOLDER_CHANNELS}/{self.channel_id}/client_secrets.json'
-            os.rename(self.client_secrets_file, new_destination)
-            self.client_secrets_file = new_destination
+    def _define_client_secrets_file(self):
 
-        else:
-            self._get_authenticated_from_file()
+        if self.case_first_time_reuse_secrets:
+            return f'{FOLDER_CHANNELS}/{self.channel_id_secrets}/client_secrets.json'
+        elif self.case_first_time_new_secrets:
+            return f'{FOLDER_CHANNELS}/new_channel/client_secrets.json'
+        elif self.case_already_stored:
+            return f'{FOLDER_CHANNELS}/{channel_id}/client_secrets.json'
 
     def _get_authenticated_first_time(self):
 
@@ -71,11 +75,26 @@ class Authentication:
         # Create a new folder for the channel
         Path(f'{FOLDER_CHANNELS}/{self.channel_id}').mkdir(parents=True, exist_ok=True)
 
-        # Save the credentials in a file called OAUTH_CREDENTIALS
+        # Save the OAuth credentials in a file called OAUTH_CREDENTIALS
         self.oauth_credentials_file = f'{FOLDER_CHANNELS}/{self.channel_id}/OAUTH_CREDENTIALS'
 
         with open(self.oauth_credentials_file, 'wb') as f:
             pickle.dump(self.credentials, f)
+
+    def _move_client_secrets_file(self):
+
+        new_destination = f'{FOLDER_CHANNELS}/{self.channel_id}/client_secrets.json'
+
+        # In this case we move the file
+        if self.case_first_time_new_secrets:
+            os.rename(self.client_secrets_file, new_destination)
+
+        # In this case we copy the file
+        elif self.case_first_time_reuse_secrets:
+            copyfile(self.client_secrets_file, new_destination)
+
+        # Redefine the new location
+        self.client_secrets_file = new_destination
 
     def _get_authenticated_from_file(self):
 
