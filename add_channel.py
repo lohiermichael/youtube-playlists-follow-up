992c253da0766483c117e87c01492f636cf45cd3
Michael Lohier <lohiermichael@gmail.com>
2020-08-27 13:00:28 +0800

    Clean add_channel.py file

--- add_channel.py
+++ add_channel.py
@@ -1,5 +1,13 @@
+import os
+
 from data_management import add_new_channel, initialize_folders
 
 if __name__ == "__main__":
     initialize_folders()
-    add_new_channel(mine=True, new_client_secrets=True)
+
+    # You need to move the client secrets under channel/new_channel to run this
+    # add_new_channel(mine=True, new_client_secrets=True)
+
+    add_new_channel(mine=True,
+                    new_client_secrets=False,
+                    channel_id_secrets=os.environ['MAIN_CHANNEL_ID'])
