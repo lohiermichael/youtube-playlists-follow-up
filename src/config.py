daf18f0fc776c382c5304214f5244a7f8d8edc98
Michael Lohier <lohiermichael@gmail.com>
2020-09-01 09:00:01 +0800

    Move code into youtube_playlists_follow_up folder

--- src/config.py
+++
@@ -1,3 +0,0 @@
-FOLDER_UPDATES = './src/updates'
-FOLDER_CHANNELS = './src/channels'
-FOLDER_LOGS = './src/logs'
---
+++ youtube_playlists_follow_up/src/config.py
@@ -0,0 +1,3 @@
+FOLDER_UPDATES = './youtube_playlists_follow_up/src/updates'
+FOLDER_CHANNELS = './youtube_playlists_follow_up/src/channels'
+FOLDER_LOGS = './youtube_playlists_follow_up/src/logs'
